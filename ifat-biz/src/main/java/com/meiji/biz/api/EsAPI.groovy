659d3d4ad7712bdd8132fca179f3dc39aaeb060a
likeguang <myqalkg@dingtalk.com>
2021-05-18 09:29:15 +0800

    删除非必要依赖

--- ifat-biz/src/main/java/com/meiji/biz/api/EsAPI.groovy
+++ ifat-biz/src/main/java/com/meiji/biz/api/EsAPI.groovy
@@ -5,5 +5,5 @@ import org.elasticsearch.client.transport.TransportClient
 
 class EsAPI {
     @Lazy
-    static TransportClient transportClient = FactorySupport.createEs("es-miyuan")
+    static TransportClient transportClient = FactorySupport.createHighLevelEs("es-miyuan")
 }

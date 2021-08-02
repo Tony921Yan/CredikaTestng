d2e9051b2edc3477cb3fcfa77ca631a07fa2267e
likeguang <myqalkg@dingtalk.com>
2021-08-02 10:55:23 +0800

    修复地址不存在问题

--- ifat-biz/src/main/java/com/meiji/biz/request/http/shop/dealer/MsgTemplate.groovy
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/shop/dealer/MsgTemplate.groovy
@@ -1,26 +1,27 @@
 package com.meiji.biz.request.http.shop.dealer
 
 import com.meiji.biz.request.http.shop.ShopGet
+import com.meiji.biz.request.http.shop.ShopPost
 import com.miyuan.ifat.support.test.TestContext
 
-class MsgTemplate extends ShopGet {
+class MsgTemplate extends ShopPost {
     {
         super.api = "dealer/msgTemplate"
         super.params =  [ "list"]
 //        super.preInvoke = "com.miyuan.request.api.goods.CenterSearch"
     }
 
-    ShopGet invoke(TestContext testContext) {
+    ShopPost invoke(TestContext testContext) {
         super.invoke(testContext)
         return this
     }
 
-    ShopGet preInvoke(TestContext testContext){
+    ShopPost preInvoke(TestContext testContext){
         super.preInvoke(testContext)
         return this
     }
 
-    ShopGet baseAssert(TestContext testContext){
+    ShopPost baseAssert(TestContext testContext){
         super.baseAssert(testContext)
     }
 }

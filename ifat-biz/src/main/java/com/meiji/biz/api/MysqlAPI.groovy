01fdb9f313b4eec08d4d6f130c7fea5d1dc92a9b
likeguang <myqalkg@dingtalk.com>
2021-08-03 15:55:00 +0800

    商品图片监控

--- ifat-biz/src/main/java/com/meiji/biz/api/MysqlAPI.groovy
+++ ifat-biz/src/main/java/com/meiji/biz/api/MysqlAPI.groovy
@@ -6,5 +6,4 @@ import groovy.sql.Sql
 class MysqlAPI {
     @Lazy
     static Sql platformGoodsSql = FactorySupport.createSql("mysql-platform-goods")
-    static Sql mallSql = FactorySupport.createSql()
 }
---
+++ ifat-test/src/main/java/com/meiji/test/debug/ItemPictureTest.groovy
@@ -0,0 +1,57 @@
+package com.meiji.test.debug
+
+import com.meiji.biz.api.MysqlAPI
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.vo.Record
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+
+class ItemPictureTest extends BaseTest {
+    @Test(dataProvider = "data",groups = ["prod"])
+    void test(TestContext testContext){
+        String url = testContext.url
+        InputStream inputStream = new URL(url).openStream()
+        BufferedImage bufferedImage = ImageIO.read(inputStream)
+
+        InputStream inputStream2 = new URL(url).openStream()
+        byte[] array = new byte[1024];
+        int size = 0;
+        int length = 0;
+        while ((length = inputStream2.read(array)) != -1) {
+            size += length;
+        }
+        size = Math.round(size/1024)
+        testContext.appendLog(new Record("商品spuCode",testContext.spuCode))
+        testContext.appendLog(new Record("图片路径",testContext.url))
+        testContext.appendLog(new Record("图片大小",size))
+        testContext.appendLog(new Record("图片尺寸",bufferedImage.getWidth()+"*"+bufferedImage.getHeight()))
+        int picSize = bufferedImage.getWidth() * bufferedImage.getHeight()
+        if(picSize >= 640000) {
+            assert size < 500,"spuCode:" + testContext.spuCode
+        }else if(picSize >= 160000){
+            assert size < 400,"spuCode:" + testContext.spuCode
+        } else if(picSize >= 40000){
+            assert size < 300,"spuCode:" + testContext.spuCode
+        }
+        inputStream.close()
+        inputStream2.close()
+    }
+
+    @DataProvider
+    TestContext[] data(){
+        List list = new ArrayList()
+        List  picList = MysqlAPI.platformGoodsSql.rows("select pic.url as url,spu.code as spuCode from goods_pic pic left join goods_spu spu on pic.spu_id = spu.id where pic.is_delete = 0 and spu.code is not null order by pic.gmt_modified desc limit 300")
+        picList.forEach {
+            TestContext testContext = new TestContext()
+            testContext.put("spuCode",it.spuCode)
+            testContext.put("url",it.url)
+            testContext.put("description",it.url)
+            list.add(testContext)
+        }
+        return list
+    }
+}

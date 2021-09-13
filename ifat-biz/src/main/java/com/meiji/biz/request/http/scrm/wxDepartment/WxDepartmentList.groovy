8db97099a37b866d04a29e0bc4d66c50efffa3e2
骆志敏 <243688409@qq.com>
2021-09-13 10:47:26 +0800

    微信部门列表

---
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/scrm/wxDepartment/WxDepartmentList.groovy
@@ -0,0 +1,15 @@
+package com.meiji.biz.request.http.scrm.wxDepartment
+
+import com.meiji.biz.request.http.scrm.ScrmGet
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/13-10:43
+ */
+class WxDepartmentList extends ScrmGet{
+    {
+        super.api = "scrm/wxDepartment/list"
+        super.params=[]
+    }
+}

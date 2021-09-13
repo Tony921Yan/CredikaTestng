555718101eb988d014881cd18f828edd69b531be
骆志敏 <243688409@qq.com>
2021-09-13 11:40:57 +0800

    配置成员

---
+++ ifat-test/src/main/java/com/meiji/test/http/scrm/employee/EmployeeConfigTest.groovy
@@ -0,0 +1,22 @@
+package com.meiji.test.http.scrm.employee
+
+import com.meiji.biz.request.http.scrm.employee.EmployeeConfig
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.Test
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/13-11:37
+ */
+class EmployeeConfigTest extends BaseTest {
+    EmployeeConfig employee = new EmployeeConfig()
+
+    @Test(description = "配置成员", groups = ["prod", "uat"], testName = "empConfig",
+            dataProvider = "dataProvider", dataProviderClass = TestData.class)
+    public void test(TestContext testContext) {
+        employee.invoke(testContext).baseAssert(testContext)
+    }
+}

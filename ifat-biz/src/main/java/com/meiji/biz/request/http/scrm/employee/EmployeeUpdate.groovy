2c8f4e1dcf7ee9361b816a9fda7bc7836202a329
骆志敏 <243688409@qq.com>
2021-09-17 10:22:06 +0800

    修改成员角色

---
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/scrm/employee/EmployeeUpdate.groovy
@@ -0,0 +1,19 @@
+package com.meiji.biz.request.http.scrm.employee
+
+import com.meiji.biz.request.http.scrm.ScrmPost
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/17-10:18
+ */
+class EmployeeUpdate extends ScrmPost{
+    {
+        super.api = "scrm/employee/update"
+        super.params = ["employeeId",
+                        "employeeIds",
+                        "mobile",
+                        "newRoleId",
+                        "oldRoleId"]
+    }
+}

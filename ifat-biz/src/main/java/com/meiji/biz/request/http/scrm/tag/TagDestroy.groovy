b0f9173fe072ff658d8f46af8da1f5c949552208
骆志敏 <243688409@qq.com>
2021-09-17 16:14:06 +0800

    删除标签-TODO

---
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/scrm/tag/TagDestroy.groovy
@@ -0,0 +1,26 @@
+package com.meiji.biz.request.http.scrm.tag
+
+import com.meiji.biz.request.http.scrm.ScrmPost
+import com.miyuan.ifat.support.test.TestContext
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/17-16:08
+ */
+class TagDestroy extends ScrmPost{
+    {
+        super.api = "scrm/tag/destroy"
+        super.params = ["groupId",
+                        "groupName",
+                        "name",
+                        "tagId",
+                        "tagIds",
+                        "tagNames"]
+    }
+
+    @Override
+    ScrmPost specialAssert(TestContext testContext) {
+        assert testContext.getResponse().code == "05004"
+    }
+}
---
+++ ifat-test/src/main/java/com/meiji/test/http/scrm/tag/TagDestroyTest.groovy
@@ -0,0 +1,22 @@
+package com.meiji.test.http.scrm.tag
+
+import com.meiji.biz.request.http.scrm.tag.TagDestroy
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.Test
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/17-16:09
+ */
+class TagDestroyTest extends BaseTest {
+    TagDestroy tag = new TagDestroy()
+
+    @Test(description = "删除标签", groups = ["prod", "uat"], testName = "tagdel",
+            dataProvider = "dataProvider", dataProviderClass = TestData.class)
+    public void test(TestContext testContext) {
+        tag.invoke(testContext).specialAssert(testContext)
+    }
+}

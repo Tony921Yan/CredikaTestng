ef0c8fcc65bf2f29362a1976e01c7cfd160423c2
骆志敏 <243688409@qq.com>
2021-09-18 13:53:52 +0800

    新增组织

---
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/scrm/department/DepartmentAdd.groovy
@@ -0,0 +1,25 @@
+package com.meiji.biz.request.http.scrm.department
+
+import com.meiji.biz.request.http.scrm.ScrmPost
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-11:36
+ */
+class DepartmentAdd extends ScrmPost{
+    {
+        super.api = "scrm/department/add"
+        super.params=["corpId",
+                      "createUserId",
+                      "employeeId",
+                      "gmtCreate",
+                      "gmtModified",
+                      "id",
+                      "name",
+                      "parentId",
+                      "state",
+                      "updateUserId",
+                      "version"]
+    }
+}

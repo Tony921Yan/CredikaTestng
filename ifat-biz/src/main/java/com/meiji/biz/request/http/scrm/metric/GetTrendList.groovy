aee0768e8ffcbd8ac4b20cc03769f0d5f12a10f1
骆志敏 <243688409@qq.com>
2021-09-18 14:58:33 +0800

    群数据趋势

---
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/scrm/metric/GetTrendList.groovy
@@ -0,0 +1,15 @@
+package com.meiji.biz.request.http.scrm.metric
+
+import com.meiji.biz.request.http.scrm.ScrmGet
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-14:55
+ */
+class GetTrendList extends ScrmGet{
+    {
+        super.api = "scrm/group/metric/getTrendList"
+        super.params = ["firstDepartment", "lastDepartment", "metricCreate", "name", "secondDepartment", "shopId", "timeType"]
+    }
+}

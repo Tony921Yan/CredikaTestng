487233f8b00ebd19ce7f16e6fedb3be1ca5a13ed
骆志敏 <243688409@qq.com>
2021-09-18 16:35:15 +0800

    门店员工群关联

---
+++ ifat-test/src/main/java/com/meiji/test/http/scrm/shop/ShopRelateTest.groovy
@@ -0,0 +1,22 @@
+package com.meiji.test.http.scrm.shop
+
+import com.meiji.biz.request.http.scrm.shop.ShopRelate
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.Test
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-16:33
+ */
+class ShopRelateTest extends BaseTest {
+    ShopRelate shop = new ShopRelate()
+
+    @Test(description = "门店员工群关联", groups = ["prod", "uat"], testName = "shoprelate",
+            dataProvider = "dataProvider", dataProviderClass = TestData.class)
+    public void test(TestContext testContext) {
+        shop.invoke(testContext).baseAssert(testContext)
+    }
+}

fd53b497ac4ab7ad933ff307ae1881cd5a0fc288
骆志敏 <243688409@qq.com>
2021-09-18 11:35:58 +0800

    编辑素材

---
+++ ifat-test/src/main/java/com/meiji/test/http/scrm/material/MaterialUpdateTest.groovy
@@ -0,0 +1,22 @@
+package com.meiji.test.http.scrm.material
+
+import com.meiji.biz.request.http.scrm.material.MaterialUpdate
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.Test
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-11:31
+ */
+class MaterialUpdateTest extends BaseTest {
+    MaterialUpdate material = new MaterialUpdate()
+
+    @Test(description = "编辑素材", groups = ["prod", "uat"], testName = "materialupdate",
+            dataProvider = "dataProvider", dataProviderClass = TestData.class)
+    public void test(TestContext testContext) {
+        material.invoke(testContext).baseAssert(testContext)
+    }
+}

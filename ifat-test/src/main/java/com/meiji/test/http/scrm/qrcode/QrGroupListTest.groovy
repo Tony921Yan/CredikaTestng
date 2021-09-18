712feca01ed797bee6b4f64113ce0d1b3befc6e5
骆志敏 <243688409@qq.com>
2021-09-18 14:31:51 +0800

    群活码列表

---
+++ ifat-test/src/main/java/com/meiji/test/http/scrm/qrcode/QrGroupListTest.groovy
@@ -0,0 +1,22 @@
+package com.meiji.test.http.scrm.qrcode
+
+import com.meiji.biz.request.http.scrm.qrcode.QrGroupList
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.Test
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-14:29
+ */
+class QrGroupListTest extends BaseTest {
+    QrGroupList qr = new QrGroupList()
+
+    @Test(description = "群活码列表", groups = ["prod", "uat"], testName = "qrlist",
+            dataProvider = "dataProvider", dataProviderClass = TestData.class)
+    public void test(TestContext testContext) {
+        qr.invoke(testContext).baseAssert(testContext)
+    }
+}

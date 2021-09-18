092036942242fe1b45fa80551672a6541988aa44
骆志敏 <243688409@qq.com>
2021-09-18 16:22:14 +0800

    更新角色菜单授权

---
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/scrm/role/UpdateGrantRoleMenu.groovy
@@ -0,0 +1,20 @@
+package com.meiji.biz.request.http.scrm.role
+
+import com.meiji.biz.request.http.scrm.ScrmPost
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-16:18
+ */
+class UpdateGrantRoleMenu extends ScrmPost {
+    {
+        super.api = "scrm/role/update_grant_role_menu"
+        super.params = ["gmtCreate",
+                        "gmtModified",
+                        "id",
+                        "menuIdList",
+                        "roleId",
+                        "state"]
+    }
+}

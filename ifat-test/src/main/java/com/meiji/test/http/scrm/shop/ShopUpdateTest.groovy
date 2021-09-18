56939d8a0e7b682d5486032c5c3b41177f9544df
骆志敏 <243688409@qq.com>
2021-09-18 16:42:03 +0800

    编辑门店

---
+++ ifat-test/src/main/java/com/meiji/test/http/scrm/shop/ShopUpdateTest.groovy
@@ -0,0 +1,22 @@
+package com.meiji.test.http.scrm.shop
+
+import com.meiji.biz.request.http.scrm.shop.ShopUpdate
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.Test
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-16:38
+ */
+class ShopUpdateTest extends BaseTest {
+    ShopUpdate shop = new ShopUpdate()
+
+    @Test(description = "编辑门店", groups = ["prod", "uat"], testName = "shopupdate",
+            dataProvider = "dataProvider", dataProviderClass = TestData.class)
+    public void test(TestContext testContext) {
+        shop.invoke(testContext).baseAssert(testContext)
+    }
+}

0659ec120bef0a80e4ed4d1f4784bf8bc25f466a
骆志敏 <243688409@qq.com>
2021-09-18 16:11:39 +0800

    角色菜单授权

---
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/scrm/role/GrantRoleMenu.groovy
@@ -0,0 +1,26 @@
+package com.meiji.biz.request.http.scrm.role
+
+import com.meiji.biz.request.http.scrm.ScrmPost
+import com.miyuan.ifat.support.test.TestContext
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-15:58
+ */
+class GrantRoleMenu extends ScrmPost{
+    {
+        super.api = "scrm/role/grant_role_menu"
+        super.params = ["gmtCreate",
+                        "gmtModified",
+                        "id",
+                        "menuIdList",
+                        "roleId",
+                        "state"]
+    }
+
+    @Override
+    ScrmPost baseAssert(TestContext testContext) {
+        assert testContext.getResponse().code == "05000"
+    }
+}
---
+++ ifat-test/src/main/java/com/meiji/test/http/scrm/role/GrantRoleMenuTest.groovy
@@ -0,0 +1,22 @@
+package com.meiji.test.http.scrm.role
+
+import com.meiji.biz.request.http.scrm.role.GrantRoleMenu
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.Test
+
+/**
+ *
+ @author s1mple
+ @create 2021/9/18-16:00
+ */
+class GrantRoleMenuTest extends BaseTest {
+    GrantRoleMenu role = new GrantRoleMenu()
+
+    @Test(description = "角色菜单授权", groups = ["prod", "uat"], testName = "grantrolemenu",
+            dataProvider = "dataProvider", dataProviderClass = TestData.class)
+    public void test(TestContext testContext) {
+        role.invoke(testContext).baseAssert(testContext)
+    }
+}

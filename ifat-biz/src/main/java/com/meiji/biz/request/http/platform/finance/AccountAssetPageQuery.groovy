cce1615ea86928b7aaa654dc669ae6bd04496b0b
likeguang <myqalkg@dingtalk.com>
2021-09-24 14:08:30 +0800

    更新财务数据：拉取最新100条资产数据，过滤测试环境数据，新增不存在表allAccountUpdateData_platform.xml的资产数据

--- ifat-biz/src/main/java/com/meiji/biz/request/http/platform/finance/AccountAssetPageQuery.groovy
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/platform/finance/AccountAssetPageQuery.groovy
@@ -26,44 +26,70 @@ class AccountAssetPageQuery extends PlatformPost{
     }
 
     PlatformPost specialAssert(TestContext testContext){
-        List<Map> apiResult = testContext.getResponse().data.getAt("dataList")
-        int count =0
-        for (i in 0..<apiResult.size()) {
-            Map map = apiResult.get(i)
-            Integer totalIncome = map.get("totalIncome")
-            Integer withdrawAmount = map.get("withdrawAmount")
-            Integer unSettledAmount = map.get("unSettledAmount")
-            Integer alreadyWithdrawAmount
-            String accountName = map.get("accountName")
-            if(map.get("alreadyWithdrawAmount") == null){alreadyWithdrawAmount =0
-            }else{alreadyWithdrawAmount = map.get("alreadyWithdrawAmount")}
-
-            if(totalIncome==withdrawAmount+unSettledAmount+alreadyWithdrawAmount){
-                assert totalIncome==withdrawAmount+unSettledAmount+alreadyWithdrawAmount
-                testContext.appendLog(new Record("以下供应商/小B数据相等（不需要补差）",accountName))}
-            else {
-                Map allAccountUpdateData = TestData.getTestData("allAccountUpdateData_platform")
-                Iterator<Map.Entry<String, String>> it = allAccountUpdateData.entrySet().iterator()
-                Integer updateValue
-                while (it.hasNext()) {
-                    Map.Entry<String, String> entry = it.next()
-//                System.out.println("key="+entry.getKey()+","+"value="+entry.getValue())
-                    if (map.get("accountCode") == entry.getKey()) {
-                        updateValue = Integer.valueOf(entry.getValue().toString())
-                        System.out.println("updateValue：" + updateValue)
-                        if (updateValue == (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount)) {
-                            assert updateValue == (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount)
-                            testContext.appendLog(new Record("以下供应商/小B数据修正补差相等（需要补差才相等）", accountName))
-                        } else {
-                            assert updateValue != (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount)
-                            testContext.appendLog(new Record("以下供应商/小B数据修正补差后仍不相等", accountName))
-                        }
-                    }
+        List<Map> apiResult = testContext.getResponse().data.dataList
+        Map allAccountUpdateData = TestData.getTestData("allAccountUpdateData_platform")
+        List errorList = new ArrayList()
+        for (Map map:apiResult){
+            Integer totalIncome = Integer.valueOf(map.get("totalIncome").toString())
+            Integer withdrawAmount = Integer.valueOf(map.get("withdrawAmount").toString())
+            Integer unSettledAmount = Integer.valueOf(map.get("unSettledAmount").toString())
+            Integer alreadyWithdrawAmount = map.get("alreadyWithdrawAmount") == null ? 0 : Integer.valueOf(map.get("alreadyWithdrawAmount"))
+            String accountCode = map.get("accountCode")
+            Integer updateValue = allAccountUpdateData.get(accountCode)==null ? 0 :Integer.valueOf(allAccountUpdateData.get(accountCode).toString())
+            if(updateValue != (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount)){
+                if(accountCode == "shop112104022025"){
+                    assert updateValue != (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount)
+                }else{
+                    Map msg = new HashMap()
+                    msg.put("accountCode",accountCode)
+                    msg.put("diff",totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount-updateValue)
+                    errorList.add(msg)
+                    testContext.appendLog(new Record("供应商/小B数据修正补差后仍不相等", msg))
                 }
+
             }
-            count++
-            }
-        testContext.appendLog(new Record("统计供应商/小B的数据条数", count))
+           // assert updateValue == (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount),"供应商/小B数据修正补差后仍不相等"+map.get("accountCode")
+        }
+        assert errorList.size() == 0
+
+//        int count =0
+//
+//            for (i in 0..<apiResult.size()) {
+//            Map map = apiResult.get(i)
+//            Integer totalIncome = map.get("totalIncome")
+//            Integer withdrawAmount = map.get("withdrawAmount")
+//            Integer unSettledAmount = map.get("unSettledAmount")
+//            Integer alreadyWithdrawAmount
+//            String accountName = map.get("accountName")
+//            if(map.get("alreadyWithdrawAmount") == null){alreadyWithdrawAmount =0
+//            }else{alreadyWithdrawAmount = map.get("alreadyWithdrawAmount")}
+//
+//            if(totalIncome==withdrawAmount+unSettledAmount+alreadyWithdrawAmount){
+//                assert totalIncome==withdrawAmount+unSettledAmount+alreadyWithdrawAmount
+//                testContext.appendLog(new Record("以下供应商/小B数据相等（不需要补差）",accountName))}
+//            else {
+//                Map allAccountUpdateData = TestData.getTestData("allAccountUpdateData_platform")
+//                Iterator<Map.Entry<String, String>> it = allAccountUpdateData.entrySet().iterator()
+//                Integer updateValue
+//                while (it.hasNext()) {
+//                    Map.Entry<String, String> entry = it.next()
+////                System.out.println("key="+entry.getKey()+","+"value="+entry.getValue())
+//                    if (map.get("accountCode") == entry.getKey()) {
+//                        updateValue = Integer.valueOf(entry.getValue().toString())
+//                        System.out.println("updateValue：" + updateValue)
+//                        if (updateValue == (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount)) {
+//                            assert updateValue == (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount)
+//                            testContext.appendLog(new Record("以下供应商/小B数据修正补差相等（需要补差才相等）", accountName))
+//                        } else {
+//                            assert updateValue != (totalIncome - withdrawAmount - unSettledAmount - alreadyWithdrawAmount)
+//                            testContext.appendLog(new Record("以下供应商/小B数据修正补差后仍不相等", accountName))
+//                        }
+//                    }
+//                }
+//            }
+//            count++
+//            }
+//        testContext.appendLog(new Record("统计供应商/小B的数据条数", count))
         }
 
     PlatformPost getallAccountData(TestContext testContext){

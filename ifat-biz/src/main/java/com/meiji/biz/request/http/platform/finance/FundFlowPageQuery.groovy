438bbae0d2304dc6b37be9a3d35e6dc0954ff680
cuiluoshi <cuiluoshi@dingtalk.com>
2021-09-27 23:52:59 +0800

    财务模块：断言5个供应商5个小B的最新20个订单的数据准确性

--- ifat-biz/src/main/java/com/meiji/biz/request/http/platform/finance/FundFlowPageQuery.groovy
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/platform/finance/FundFlowPageQuery.groovy
@@ -2,6 +2,7 @@ package com.meiji.biz.request.http.platform.finance
 
 import com.meiji.biz.request.http.platform.PlatformPost
 import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
 
 class FundFlowPageQuery extends PlatformPost{
     {
@@ -21,6 +22,22 @@ class FundFlowPageQuery extends PlatformPost{
 
     PlatformPost baseAssert(TestContext testContext){
         super.baseAssert(testContext)
+    }
+
+    PlatformPost specialAssert(TestContext testContext){
+        List<Map> apiResult = testContext.getResponse().data.dataList
+        for (Map map:apiResult){
+            Integer actuallyPaidAmount = Integer.valueOf(map.get("actuallyPaidAmount").toString())
+            Integer actuallySettleAmount = Integer.valueOf(map.get("actuallySettleAmount").toString())
+            Integer actuallyCommission = Integer.valueOf(map.get("actuallyCommission").toString())
+            Integer actuallyPlatformIncome = map.get("actuallyPlatformIncome") == null ? 0 : Integer.valueOf(map.get("actuallyPlatformIncome"))
+            Integer wechatFee = Integer.valueOf(map.get("wechatFee").toString())
+            Integer totalRefundAmount = Integer.valueOf(map.get("totalRefundAmount").toString())
+            Integer taxes = Integer.valueOf(map.get("totalRefundAmount").toString())
+            Integer subsidyAmount = Integer.valueOf(map.get("subsidyAmount").toString())
+            assert actuallyPaidAmount == (actuallySettleAmount + actuallyCommission + actuallyPlatformIncome +wechatFee +
+                    totalRefundAmount +taxes +subsidyAmount)
+        }
     }
 
 }
--- ifat-test/src/main/java/com/meiji/test/http/platform/finance/FundFlowPageQueryTest.groovy
+++ ifat-test/src/main/java/com/meiji/test/http/platform/finance/FundFlowPageQueryTest.groovy
@@ -9,9 +9,75 @@ import org.testng.annotations.Test
 
 class FundFlowPageQueryTest extends BaseTest {
     FundFlowPageQuery fundFlowPageQuery = new FundFlowPageQuery()
-    @Test(description = "资金记录分页查询 fundFlowPageQuery" ,groups = ["prod","uat"],testName = "fundFlowPageQuery",
+    @Test(description = "深圳市榴芒一刻食品有限公司--最新20条数据核对 fundFlowPageQuerySettlementFinish" ,groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish",
             dataProvider = "dataProvider",dataProviderClass = TestData.class)
     public void fundFlowPageQuery(TestContext testContext){
         fundFlowPageQuery.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    FundFlowPageQuery fundFlowPageQuerySettlementFinish = new FundFlowPageQuery()
+    @Test(description = "广州猎尚信息科技有限公司--最新20条数据核对 fundFlowPageQuerySettlementFinish1",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish1",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    @Test(description = "大埔县兴瑞现代农业发展有限公司--最新20条数据核对 fundFlowPageQuerySettlementFinish2",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish2",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish2(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    @Test(description = "云南山味集商贸有限公司--最新20条数据核对 fundFlowPageQuerySettlementFinish3",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish3",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish3(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    @Test(description = "广东邓老凉茶药业集团股份有限公司--最新20条数据核对 fundFlowPageQuerySettlementFinish4",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish4",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish4(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    @Test(description = "依山而立自用推荐--最新20条数据核对 fundFlowPageQuerySettlementFinish5",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish5",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish5(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    @Test(description = "流水禅生8657的小店--最新20条数据核对 fundFlowPageQuerySettlementFinish6",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish6",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish6(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    @Test(description = "启德的小店--最新20条数据核对 fundFlowPageQuerySettlementFinish7",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish7",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish7(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    @Test(description = "perry的心选小店铺--最新20条数据核对 fundFlowPageQuerySettlementFinish8",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish8",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish8(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
+    }
+    @Test(description = "栖霞春天E8928的小店--最新20条数据核对 fundFlowPageQuerySettlementFinish9",groups = ["prod","uat"],
+            testName = "fundFlowPageQuerySettlementFinish9",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void fundFlowPageQuerySettlementFinish9(TestContext testContext){
+        fundFlowPageQuerySettlementFinish.invoke(testContext).baseAssert(testContext)
+        fundFlowPageQuerySettlementFinish.specialAssert(testContext)
     }
 }

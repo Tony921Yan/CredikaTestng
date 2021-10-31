b282272bc403cddc2038fbab51749f4a22bd7498
likeguang <myqalkg@dingtalk.com>
2021-10-31 10:31:11 +0800

    修改签名

--- ifat-biz/src/main/java/com/meiji/biz/util/CpsSign.groovy
+++ ifat-biz/src/main/java/com/meiji/biz/util/CpsSign.groovy
@@ -1,5 +1,7 @@
 package com.meiji.biz.util
 
+import org.codehaus.groovy.util.StringUtil
+
 import javax.xml.bind.DatatypeConverter
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
@@ -10,34 +12,20 @@ class CpsSign {
     private static String secret = "7BF8D7BC15"
     static String sign(Map map){
         StringBuffer paramStr = new StringBuffer(secret);
-        for (String key : map.keySet()) {
-            paramStr.append(key);
-            paramStr.append("=");
-            paramStr.append(map.get(key));
-            paramStr.append("&");
+        Set<Map.Entry<String, Object>> entries = map.entrySet();
+        Iterator<Map.Entry<String, Object>> iterator = entries.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Object> entry = iterator.next();
+            if (!(null == entry.getValue()||entry.toString().length()==0)) {
+                paramStr.append(entry.getKey());
+                paramStr.append("=");
+                paramStr.append(entry.getValue());
+                if(iterator.hasNext())
+                    paramStr.append("&");
+            }
         }
-        paramStr.substring(0, paramStr.length()-1);
         paramStr.append(secret);
         String sign = MD5Utils.MD5Encode(URLEncoder.encode(paramStr.toString(),"UTF-8"), "UTF-8");
         return sign.toUpperCase();
-
-//        final String strA = map.entrySet().stream()
-//                .sorted(Map.Entry.comparingByKey())
-//                .map(e -> String.format("%s=%s", ((Map.Entry) e).getKey(), ((Map.Entry) e).getValue()))
-//                .reduce((x, y) -> x + "&" + y)
-//                .orElse("");
-//
-//        // 3、字符串A的前后加上secret组成字符串B
-//        String strB = secret + strA + secret;
-
-        // 4、对字符串B进行encode后MD5加密，取大写，得到签名sign
-        //String sign = md5(strB);
-        //return sign
-    }
-
-    private static String md5(String content) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(URLEncoder.encode(content, StandardCharsets.UTF_8.name()).getBytes(StandardCharsets.UTF_8));
-        return DatatypeConverter.printHexBinary(md.digest());
     }
 }

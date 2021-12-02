f604b8c9a9e1d3266d7d30c05dcce0ccce47701e
cuiluoshi <cuiluoshi@dingtalk.com>
2021-12-02 14:50:41 +0800

    update

--- ifat-test/src/main/java/com/meiji/test/http/mall/shopGoods/GetSpuBuyerShowTest.groovy
+++ ifat-test/src/main/java/com/meiji/test/http/mall/shopGoods/GetSpuBuyerShowTest.groovy
@@ -14,7 +14,7 @@ import org.testng.annotations.Test
  */
 class GetSpuBuyerShowTest extends BaseTest{
     GetSpuBuyerShow getSpuBuyerShow = new GetSpuBuyerShow()
-    @Test(description = "查询精选晒单 listPostContent" ,groups = ["prod","uat"],testName = "getSpuBuyerShow",
+    @Test(description = "查询精选晒单 getSpuBuyerShow" ,groups = ["prod","uat"],testName = "getSpuBuyerShow",
             dataProvider = "dataProvider",dataProviderClass = TestData.class)
     public void getSpuBuyerShow(TestContext testContext){
         getSpuBuyerShow.invoke(testContext).baseAssert(testContext)

0fe3d9c81d17930f0786a2149fb30e12f8b21d97
likeguang <myqalkg@dingtalk.com>
2021-12-07 09:23:10 +0800

    debug代码上传记录

--- ifat-test/src/main/java/com/meiji/test/debug/UtilDebug.groovy
+++ ifat-test/src/main/java/com/meiji/test/debug/UtilDebug.groovy
@@ -17,27 +17,33 @@ class UtilDebug {
         println(DateUtil.dateAdd(new Date(),Calendar.DAY_OF_YEAR,1))
     }
 
-    @Test(dataProvider = "getData")
-    void testPost(Object o){
-        String url = "http://127.0.0.1:8080/mall/shop/goods/findShopGoodsDetail"
-        //String url = "https://test.meiji8888.com/mall/shop/goods/findShopGoodsDetail"
+    @Test
+    void testPost(){
+        //String url = "http://127.0.0.1:8080/mall/shop/goods/findShopGoodsDetail"
+        String url = "https://mall.meiji8888.com/shopGoods/findShopGoodsDetail"
         TestContext testContext = TestData.getCommonData()
         Map head = new HashMap()
         head.put("timestamp",testContext.get("timestamp"))
         head.put("nonce",testContext.get("nonce"))
         head.put("Content-Type",testContext.get("Content-Type"))
         head.put("isGrayRelease",true)
-//        UUID uuid = UUID.randomUUID();
-//        head.put("isUserLogTracing",true)
-//        head.put("userLogTracingTag",uuid)
-        head.put("userId",10014)
+        UUID uuid = UUID.randomUUID();
+        head.put("isUserLogTracing",true)
+        head.put("userLogTracingTag",uuid)
+        head.put("userId",10053)
         TreeMap req = new TreeMap()
         req.put("spuId","1419204305616928")
         req.put("shopId",1405965664518176)
-        println("=============== start ========================================")
-        String res = HttpUtil.post(url, head, req)
-        println(JsonUtil.prettyJson(res))
-        println("=============== end ========================================")
+        for(int i=0;i<1;i++) {
+            println("=============== start ========================================")
+            println("=============== head ========================================")
+            println(JsonUtil.prettyJson(head))
+            println("=============== req ========================================")
+            println(JsonUtil.prettyJson(req))
+            String res = HttpUtil.post(url, head, req)
+            println(JsonUtil.prettyJson(res))
+            println("=============== end ========================================")
+        }
 
     }
     @DataProvider

29d5cfaf536c236e68deb089bd1100e036aef5a0
cuiluoshi <cuiluoshi@dingtalk.com>
2022-02-07 11:42:08 +0800

    V2.5 多个接口新增

---
+++ ifat-test/src/main/java/com/meiji/test/http/mall/content/SaveBuyerShowTest.groovy
@@ -0,0 +1,21 @@
+package com.meiji.test.http.mall.content
+
+import com.meiji.biz.request.http.mall.content.SaveBuyerShow
+import com.miyuan.ifat.support.test.BaseTest
+import com.miyuan.ifat.support.test.TestContext
+import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.Test
+
+/**
+ * @author Remy*
+ * @date 2022/02/05 22:56
+ * @Vession V2.5
+ */
+class SaveBuyerShowTest extends BaseTest{
+    SaveBuyerShow saveBuyerShow = new SaveBuyerShow()
+    @Test(description = "保存晒单（对于未购买进行晒单无效）saveBuyerShow",groups = ["uat","prod"],testName = "saveBuyerShow",
+            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    public void saveBuyerShow(TestContext testContext){
+        saveBuyerShow.invoke(testContext).baseAssert(testContext)
+    }
+}

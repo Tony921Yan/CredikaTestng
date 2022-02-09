178e6b8c9fa4cb8bdd2b5b7dcd1a35ff2161bf54
likeguang <myqalkg@dingtalk.com>
2022-02-09 11:04:25 +0800

    指定文件路径

--- ifat-test/src/main/java/com/meiji/test/http/mall/home/GetChannelDataTest.groovy
+++ ifat-test/src/main/java/com/meiji/test/http/mall/home/GetChannelDataTest.groovy
@@ -1,9 +1,12 @@
 package com.meiji.test.http.mall.home
 
 import com.meiji.biz.request.http.mall.home.GetChannelData
+import com.miyuan.ifat.support.annotation.Data
 import com.miyuan.ifat.support.test.BaseTest
 import com.miyuan.ifat.support.test.TestContext
 import com.miyuan.ifat.support.test.TestData
+import org.testng.annotations.CustomAttribute
+import org.testng.annotations.Parameters
 import org.testng.annotations.Test
 
 /**
@@ -14,8 +17,8 @@ import org.testng.annotations.Test
  */
 class GetChannelDataTest extends BaseTest{
     GetChannelData getChannelData = new GetChannelData()
-    @Test(description = "获取频道数据 getChannelData" ,groups = ["prod","uat"],testName = "getChannelData",
-            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    @Test(description = "获取频道数据 getChannelData" ,groups = ["prod","uat"],dataProvider = "dataProvider",dataProviderClass = TestData.class)
+    @Data(dataFile = "mall/getChannelData.xml",testName = "getChannelData",commonFile = "common.xml")
     public void getChannelData(TestContext testContext){
         getChannelData.invoke(testContext).baseAssert(testContext)
     }

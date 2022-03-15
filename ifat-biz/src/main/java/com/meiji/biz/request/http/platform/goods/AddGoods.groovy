afddaa5ba658492ec74e454c1c705d346954fd0e
cuiluoshi <cuiluoshi@dingtalk.com>
2022-03-16 00:17:21 +0800

    中台商品添加商品修改

--- ifat-biz/src/main/java/com/meiji/biz/request/http/platform/goods/AddGoods.groovy
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/platform/goods/AddGoods.groovy
@@ -6,44 +6,42 @@ import com.miyuan.ifat.support.test.TestContext
  *
  @author Remy
  @create 2021/3/6-17:50
- @update 2021/11/25-17:50
+ @update 2021/11/25-17:50,2022/3/14-23:50
  @version v2.2
  */
 class AddGoods extends PlatformPost{
     {
-        super.api = "GoodsDetailManage/addSubmitGoods"
-        super.params = ["carouselPic",
-                        "freightTemplateId",
-                        "goodsType",
-                        "goodsVideo",
-                        "insurance",
-                        "supplierReturnAddressId",
-                        "name",
-                        "sellingPoint",
-                        "originAddress",
-                        "brandId",
-                        "frontDeskCategoryId",
-                        "backgroundCategoryId",
-                        "detail",
-                        "manyProperty",
-                        "createBy",
-                        "createByName",
-                        "updateBy",
-                        "updateByName",
-                        "supplierId",
-                        "supplierCode",
-                        "deliveryType",
-                        "isSevenDayReturn",
-                        "freAskedQuestions",
-                        "supervisorId",
-                        "isShowSupSuggest",
-                        "limitCycle",
-                        "limitNum",
-                        "isPurchaseLimits",
-                        "labelIds",
-                        "goodsCardPic",
-                        "pics",
-                        "skus",]
+        super.api = "GoodsDetailManage/addGoods"
+        super.params = ["backgroundCategoryId",
+        "brandId",
+        "buyerShowLabelList",
+        "carouselPic",
+        "deliveryType",
+        "detail",
+        "freAskedQuestions",
+        "freightTemplateId",
+        "frontDeskCategoryId",
+        "goodsCardPic",
+        "goodsType",
+        "goodsVideo",
+        "insurance",
+        "isCompensation",
+        "isPurchaseLimits",
+        "isSevenDayReturn",
+        "isShowSupSuggest",
+        "labelIds",
+        "limitCycle",
+        "limitNum",
+        "manyProperty",
+        "name",
+        "originAddress",
+        "pics",
+        "sellingPoint",
+        "skus",
+        "supervisorId",
+        "supplierCode",
+        "supplierId",
+        "supplierReturnAddressId"]
     }
 
     AddGoods invoke(TestContext testContext) {

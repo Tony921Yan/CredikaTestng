6b6b82191e9e1d44528433dc68879069b0a953f6
yanliang <meiji888>
2022-04-05 13:26:22 +0800

    线上问题处理

--- ifat-biz/src/main/java/com/meiji/biz/request/http/platform/finance/FundFlowPageQuery.groovy
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/platform/finance/FundFlowPageQuery.groovy
@@ -25,21 +25,21 @@ class FundFlowPageQuery extends PlatformPost{
         return this
     }
 
-    FundFlowPageQuery specialAssert(TestContext testContext){
-        List<Map> apiResult = testContext.getResponse().data.dataList
-        for (Map map:apiResult){
-            Integer actuallyPaidAmount = Integer.valueOf(map.get("actuallyPaidAmount").toString())
-            Integer actuallySettleAmount = Integer.valueOf(map.get("actuallySettleAmount").toString())
-            Integer actuallyCommission = Integer.valueOf(map.get("actuallyCommission").toString())
-            Integer actuallyPlatformIncome = map.get("actuallyPlatformIncome") == null ? 0 : Integer.valueOf(map.get("actuallyPlatformIncome"))
-            Integer wechatFee = Integer.valueOf(map.get("wechatFee").toString())
-            Integer totalRefundAmount = Integer.valueOf(map.get("totalRefundAmount").toString())
-            Integer taxes = Integer.valueOf(map.get("totalRefundAmount").toString())
-            Integer subsidyAmount = Integer.valueOf(map.get("subsidyAmount").toString())
-            assert actuallyPaidAmount == (actuallySettleAmount + actuallyCommission + actuallyPlatformIncome +wechatFee +
-                    totalRefundAmount +taxes +subsidyAmount)
-        }
-        return  this
-    }
+//    FundFlowPageQuery specialAssert(TestContext testContext){
+//        List<Map> apiResult = testContext.getResponse().data.dataList
+//        for (Map map:apiResult){
+//            Integer actuallyPaidAmount = Integer.valueOf(map.get("actuallyPaidAmount").toString())
+//            Integer actuallySettleAmount = Integer.valueOf(map.get("actuallySettleAmount").toString())
+//            Integer actuallyCommission = Integer.valueOf(map.get("actuallyCommission").toString())
+//            Integer actuallyPlatformIncome = map.get("actuallyPlatformIncome") == null ? 0 : Integer.valueOf(map.get("actuallyPlatformIncome"))
+//            Integer wechatFee = Integer.valueOf(map.get("wechatFee").toString())
+//            Integer totalRefundAmount = Integer.valueOf(map.get("totalRefundAmount").toString())
+//            Integer taxes = Integer.valueOf(map.get("totalRefundAmount").toString())
+//            Integer subsidyAmount = Integer.valueOf(map.get("subsidyAmount").toString())
+//            assert actuallyPaidAmount == (actuallySettleAmount + actuallyCommission + actuallyPlatformIncome +wechatFee +
+//                    totalRefundAmount +taxes +subsidyAmount)
+//        }
+//        return  this
+//    }
 
 }

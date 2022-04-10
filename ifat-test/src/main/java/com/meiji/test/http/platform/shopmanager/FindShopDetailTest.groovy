416e4a4badeb9709ef6b96ccfedc081db392503a
cuiluoshi <cuiluoshi@dingtalk.com>
2022-04-10 17:04:22 +0800

    更新中台接口若干

--- ifat-test/src/main/java/com/meiji/test/http/platform/shopmanager/FindShopDetailTest.groovy
+++ ifat-test/src/main/java/com/meiji/test/http/platform/shopmanager/FindShopDetailTest.groovy
@@ -1,4 +1,4 @@
-package com.meiji.test.http.platform.shopmanager
+package com.meiji.test.http.platform.shopManager
 import com.meiji.biz.request.http.platform.shopmanager.FindShopDetail
 import com.miyuan.ifat.support.test.BaseTest
 import com.miyuan.ifat.support.test.TestContext

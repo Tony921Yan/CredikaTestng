7dec4357bd98852e0a5ed88922b1bcb4e91aa153
yanliang <meiji888>
2022-04-15 19:46:01 +0800

    线上问题处理

--- ifat-test/src/main/java/com/meiji/test/http/platform/shopManager/UpdateBankCardInfoTest.groovy
+++ ifat-test/src/main/java/com/meiji/test/http/platform/shopManager/UpdateBankCardInfoTest.groovy
@@ -1,7 +1,6 @@
-package com.meiji.test.http.platform.shopmanager
+package com.meiji.test.http.platform.shopManager
 
 
-import com.meiji.biz.request.http.platform.shopmanager.FindShopDetail
 import com.meiji.biz.request.http.platform.shopmanager.UpdateBankCardInfo
 import com.miyuan.ifat.support.test.BaseTest
 import com.miyuan.ifat.support.test.TestContext

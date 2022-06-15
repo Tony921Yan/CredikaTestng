75a7d057cec697b35b787b539b530575f143540c
cuiluoshi <cuiluoshi@dingtalk.com>
2022-06-15 11:09:12 +0800

    V2.9.1 生产环境未上线，暂时屏蔽拼团活动

--- ifat-test/src/main/java/com/meiji/test/http/platform/orderDetailManage/GetOrderByPageTest.groovy
+++ ifat-test/src/main/java/com/meiji/test/http/platform/orderDetailManage/GetOrderByPageTest.groovy
@@ -136,11 +136,11 @@ class GetOrderByPageTest extends BaseTest {
         getOrderByPage.invoke(testContext).baseAssert(testContext)
     }
 
-    @Test(description = "获取订单数据-订单类型：拼团订单 uatGetOrderByPage",groups = ["prod","uat"],testName = "GetOrderByPage-pintuan",
-            dataProvider = "dataProvider",dataProviderClass = TestData.class)
-    public void GetOrderByPage20(TestContext testContext){
-        getOrderByPage.invoke(testContext).baseAssert(testContext)
-    }
+//    @Test(description = "获取订单数据-订单类型：拼团订单 uatGetOrderByPage",groups = ["prod","uat"],testName = "GetOrderByPage-pintuan",
+//            dataProvider = "dataProvider",dataProviderClass = TestData.class)
+//    public void GetOrderByPage20(TestContext testContext){
+//        getOrderByPage.invoke(testContext).baseAssert(testContext)
+//    }
 
 
 }

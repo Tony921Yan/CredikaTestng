78fe127739129e6c325b4e42a73c7d7b29d535b8
cuiluoshi <cuiluoshi@dingtalk.com>
2023-01-02 19:26:22 +0800

    V3.7送礼内容: 中台广告位管理；分类管理

---
+++ ifat-biz/src/main/java/com/meiji/biz/request/http/platform/activeMainManage/giftManage/QueryCircleList.groovy
@@ -0,0 +1,38 @@
+package com.meiji.biz.request.http.platform.activeMainManage.giftManage
+
+import com.meiji.biz.request.http.platform.PlatformPost
+import com.miyuan.ifat.support.test.TestContext
+
+class QueryCircleList extends PlatformPost{
+    {
+        super.api = "CircleManage/queryCircleList"
+        super.params = []
+    }
+
+    QueryCircleList invoke(TestContext testContext) {
+        super.invoke(testContext)
+        return this
+    }
+
+    QueryCircleList preInvoke(TestContext testContext){
+        super.preInvoke(testContext)
+        return this
+    }
+
+    QueryCircleList baseAssert(TestContext testContext){
+        super.baseAssert(testContext)
+        return this
+    }
+
+//    GetCategoryList afterInvoke(TestContext testContext){
+//        def apiResult = testContext.getResponse().data
+//        if(apiResult==null){
+//            println("暂无分类数据")
+//            throw new SkipException("暂无分类数据")
+//        }
+//        testContext.put("id",apiResult.dataList.get(0).get(id))
+//    }
+
+
+
+}
